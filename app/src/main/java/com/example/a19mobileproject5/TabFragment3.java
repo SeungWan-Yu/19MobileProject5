7973d14f3cc4a786f9367ebf2bf695b4a2fa1d10
S.W.LYU <seungwan225@naver.com>
2019-04-17 12:55:05 +0900

    구글맵 api 위젯 자동완성 추가 구동은 X api key 에러

--- app/src/main/java/com/example/a19mobileproject5/TabFragment3.java
+++ app/src/main/java/com/example/a19mobileproject5/TabFragment3.java
@@ -1,36 +1,83 @@
 package com.example.a19mobileproject5;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.places.*;
 
-public class TabFragment3 extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+import static android.content.ContentValues.TAG;
+
+public class TabFragment3 extends Fragment implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,GoogleApiClient.OnConnectionFailedListener {
 
     private MapView mapView = null;
-    private GoogleApiClient.Builder mGoogleApiClient;
+    private GoogleMap map;
+    LocationManager manager;
+    private Marker currentMarker = null;
+    private static final LatLng DEFAULT_LOCATION = new LatLng(37.56, 126.97);
 
 
-    public TabFragment3()
-    {
+
+    public TabFragment3() {
 
     }
 
+    public void setCurrentLocation(Location location, String markerTitle, String markerSnippet) {
+        if ( currentMarker != null ) currentMarker.remove();
+
+        if ( location != null) {
+            //현재위치의 위도 경도 가져옴
+            LatLng currentLocation = new LatLng( location.getLatitude(), location.getLongitude());
+
+            MarkerOptions markerOptions = new MarkerOptions();
+            markerOptions.position(currentLocation);
+            markerOptions.title(markerTitle);
+            markerOptions.snippet(markerSnippet);
+            markerOptions.draggable(true);
+            markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            currentMarker = this.map.addMarker(markerOptions);
+
+            this.map.moveCamera(CameraUpdateFactory.newLatLng(currentLocation));
+            return;
+        }
+
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(DEFAULT_LOCATION);
+        markerOptions.title(markerTitle);
+        markerOptions.snippet(markerSnippet);
+        markerOptions.draggable(true);
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+        currentMarker = this.map.addMarker(markerOptions);
+
+        this.map.moveCamera(CameraUpdateFactory.newLatLng(DEFAULT_LOCATION));
+    }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +93,27 @@ public class TabFragment3 extends Fragment implements OnMapReadyCallback, Google
 
         mapView = layout.findViewById(R.id.map);
         mapView.getMapAsync(this);
+
+        /*
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getActivity().getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                Location location = new Location("");
+                location.setLatitude(place.getLatLng().latitude);
+                location.setLongitude(place.getLatLng().longitude);
+
+                setCurrentLocation(location, place.getName().toString(), place.getAddress().toString());
+            }
+
+            @Override
+            public void onError(Status status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });*/
+
 
         return layout;
     }
@@ -65,7 +133,20 @@ public class TabFragment3 extends Fragment implements OnMapReadyCallback, Google
     @Override
     public void onResume() {
         super.onResume();
-        mapView.onResume();
+        //마시멜로 이상버전에서는 런타임 권한 체크여부를 확인해야 한다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // GPS 사용을 위한 권한 휙득이 되어 있지 않으면 리스너 해제하지 않는다
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        // GPS 리스너 등록
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, //위치제공자
+                3000, //변경사항 체크 주기 millisecond 단위임
+                1, //변경사항 체크 거리 meter단위
+                locationListener //나는 locationListener를 쓸꺼야
+        );
     }
 
     @Override
@@ -85,55 +166,68 @@ public class TabFragment3 extends Fragment implements OnMapReadyCallback, Google
         super.onDestroy();
         mapView.onLowMemory();
     }
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        if(mapView != null)
-        {
+        if (mapView != null) {
             mapView.onCreate(savedInstanceState);
         }
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng SEOUL = new LatLng(37.56, 126.97);
 
-        MarkerOptions markerOptions = new MarkerOptions();
+        map = googleMap;
 
-        markerOptions.position(SEOUL);
-
-        markerOptions.title("서울");
-
-        markerOptions.snippet("수도");
-
-        googleMap.addMarker(markerOptions);
-
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
-
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-
+        LatLng namsan = new LatLng(37.551968, 126.988500);
+        //좌표 적용
+        // 1.1 마커생성
+        MarkerOptions marker = new MarkerOptions();
+        marker.position(namsan); //좌표
+        marker.title("남산타워");
+        // 1.2 마커를 화면에 그림
+        map.addMarker(marker);
+        // 2. 맵의 중심을 해당 좌표로 이동                          좌표   , 줌레벨
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(namsan, 17));
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            double lng = location.getLongitude();
+            //위도
+            double lat = location.getLatitude();
+            //고도
+            double alt = location.getAltitude();
+            //정확도
+            float acc = location.getAccuracy();
+            //위치제공자(ISP)
+            String provider = location.getProvider();
 
-    }
+            //바뀐 현재 좌표
+            LatLng current = new LatLng(lat, lng);
+            //현재좌표로 카메라를 이동시킬때 // TODO 바로 여기에 전역변수 구글맵이 들어감
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 17));
+        }
 
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
 
-    }
+        }
 
-    @Override
-    public void onProviderEnabled(String provider) {
+        @Override
+        public void onProviderEnabled(String provider) {
 
-    }
+        }
 
-    @Override
-    public void onProviderDisabled(String provider) {
+        @Override
+        public void onProviderDisabled(String provider) {
 
-    }
+        }
+    };
+
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
@@ -147,6 +241,46 @@ public class TabFragment3 extends Fragment implements OnMapReadyCallback, Google
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+    private final int REQ_PERMISSION = 100;
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkPermission() {
+        //1 권한체크 - 특정권한이 있는지 시스템에 물어본다
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+        } else {
+            // 2. 권한이 없으면 사용자에 권한을 달라고 요청
+            String permissions[] = {Manifest.permission.ACCESS_FINE_LOCATION};
+            requestPermissions(permissions, REQ_PERMISSION); // -> 권한을 요구하는 팝업이 사용자 화면에 노출된다
+        }
+    }
+
+    private int checkSelfPermission(String accessFineLocation) {
+        return 0;
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQ_PERMISSION) {
+            // 3.1 사용자가 승인을 했음
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            } else {
+                cancel();
+            }
+        }
+    }
+
+    private void cancel() {
+
+        // 권한을 실행시킨다
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkPermission();
+        }
+    }
+
+    private void finish() {
 
     }
 }
